8b053ca346a7c46538a9cdf9818b09fa1d93992a
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-09-01 02:36:36 -0400

    get articles rough route working

---
+++ src/backend/lib/helpers/article-converter.js
@@ -0,0 +1,51 @@
+class ArticlesConverter {
+    constructor(rawText, userHandle) {
+        this.userHandle = userHandle;
+        this.rawText = rawText;
+        return this.niceJSONArticles;
+    }
+
+    checkForImage(imageId) {
+        return imageId
+            ? `https://miro.medium.com/max/1400/${imageId}`
+            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Pictogram_voting_question.svg/440px-Pictogram_voting_question.svg.png'
+    }
+
+    formatTags(tags) {
+        return tags.map(({ name, slug }) => ({ name, slug }))
+    }
+
+    formatArticle(rawArticle) {
+        const {
+            id: mediumID,
+            title,
+            slug,
+            uniqueSlug,
+            virtuals: { previewImage: { imageId }, tags },
+            content: { subtitle, metaDescription },
+        } = rawArticle;
+
+        const link = `https://medium.com/@${this.userHandle}/${uniqueSlug}`;
+
+        return {
+            mediumID,
+            title,
+            slug,
+            link,
+            image: this.checkForImage(imageId),
+            subtitle,
+            metaDescription,
+            tags: this.formatTags(tags),
+        };
+    };
+
+    get roughJSONArticles() {
+        return JSON.parse(this.rawText.slice(this.rawText.indexOf('{'))).payload.references.Post;
+    }
+
+    get niceJSONArticles() {
+        return Object.keys(this.roughJSONArticles).map(article => this.formatArticle(this.roughJSONArticles[article]));
+    }
+}
+
+module.exports = ArticlesConverter;
--- src/backend/lib/routes/getArticles.js
+++ src/backend/lib/routes/getArticles.js
@@ -1,9 +1,16 @@
+const nodeFetch = require('node-fetch');
+const ArticleConverter = require('../helpers/article-converter');
+
 module.exports = {
     method: 'GET',
     path: '/get-articles',
     options: {
         handler: async (request, h) => {
-            return { msg: 'get-articles' };
+            const result = await nodeFetch('https://medium.com/@mikecronin92/latest?format=json');
+            const rawText = await result.text();
+            const articles = new ArticleConverter(rawText, 'mikecronin92')
+            console.log('result: ', articles);
+            return articles;
         },
     },
 };
