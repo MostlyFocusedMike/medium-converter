c596f49a5070f48efbd81182a9fc395cee399c48
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-09-18 23:16:51 -0400

    add converter class

--- src/frontend/components/Hello.tsx
+++ src/frontend/components/Hello.tsx
@@ -2,9 +2,7 @@ import * as React from "react";
 
 export interface HelloProps { compiler: string; framework: string; }
 
-// you don't need to include the JSX.Element, that's implied
-// components have to return a JSX.Element or null
-export const Hello = (props: HelloProps): JSX.Element => {
+export const Hello = (props: HelloProps) => {
     return (
         <h1>Hello from {props.compiler} and {props.framework}!</h1>
     )
---
+++ src/frontend/utilities/RawMediumToJSON.ts
@@ -0,0 +1,73 @@
+export interface rawTagIntf {
+    name: string,
+    slug: string,
+    postCount: number,
+    metadata: object,
+    type: string,
+}
+
+export interface rawArticleIntf {
+    id: string;
+    title: string;
+    slug: string;
+    uniqueSlug: string;
+    virtuals: {
+        previewImage: {
+            imageId: string;
+        },
+        tags: rawTagIntf[];
+    };
+    content: {
+        subtitle: string;
+        metaDescription: string;
+    };
+}
+
+export class ArticlesConverter {
+    rawText: string;
+    userHandle: string;
+    constructor(rawText: string, userHandle: string) {
+        this.userHandle = userHandle;
+        this.rawText = rawText;
+    }
+
+    checkForImage = (imageId: string) => (
+        imageId
+            ? `https://miro.medium.com/max/1400/${imageId}`
+            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Pictogram_voting_question.svg/440px-Pictogram_voting_question.svg.png'
+    );
+
+    formatTags = (tags: rawTagIntf[]) => tags.map((tag) => ({ name: tag.name, slug: tag.slug }));
+
+    formatArticle = (rawArticle: rawArticleIntf) => {
+        const {
+            id: mediumID,
+            title,
+            slug,
+            uniqueSlug,
+            virtuals: { previewImage: { imageId }, tags },
+            content: { subtitle, metaDescription },
+        } = rawArticle;
+
+        const link = `https://medium.com/@${this.userHandle}/${uniqueSlug}`;
+
+        return {
+            mediumID,
+            title,
+            slug,
+            link,
+            image: this.checkForImage(imageId),
+            subtitle,
+            metaDescription,
+            tags: this.formatTags(tags),
+        };
+    };
+
+    get roughJSONArticles() {
+        return JSON.parse(this.rawText.slice(this.rawText.indexOf('{'))).payload.references.Post;
+    }
+
+    get niceJSONArticles() {
+        return Object.keys(this.roughJSONArticles).map(article => this.formatArticle(this.roughJSONArticles[article]));
+    }
+}
