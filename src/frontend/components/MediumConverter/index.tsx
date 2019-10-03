a4ac9d570c9ee7f6357fa80f04bb9094ee208207
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-10-03 00:10:19 -0400

    refactor link into own component

--- src/frontend/components/MediumConverter/index.tsx
+++ src/frontend/components/MediumConverter/index.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import './styles.css';
 import Constants from '../../../constants'
 import RawMediumToJSON, { niceJSONArticlesIntf } from '../../utilities/raw-medium-to-json';
+import { ArticleAdapter } from '../../adapters';
 import ArticlesHolder from './SubComponents/ArticlesHolder';
 import JsonArticlesCard from './SubComponents/JsonArticlesCard';
-import { ArticleAdapter } from '../../adapters';
+import MediumJsonLink from './SubComponents/MediumLink';
 
 // export const MediumConverterContainer: React.FC<PropsItf> = (props) => {
 const MediumConverter: React.FC = () => {
@@ -12,8 +13,6 @@ const MediumConverter: React.FC = () => {
     const [articles, setArticles] = useState<niceJSONArticlesIntf[]>([]);
     const [fullJson, setFullJson] = useState<any>([]); // figure out interface
 
-    // useEffect(() => { console.log('rawText: ', rawText); }, [rawText]);
-    // useEffect(() => { console.log('articles: ', articles); }, [articles]);
     useEffect(() => {
         ArticleAdapter.getAllWithTags().then(setFullJson);
      }, []);
@@ -39,12 +38,7 @@ const MediumConverter: React.FC = () => {
     return (
         <div id='medium-converter'>
             <h1>Raw Medium Text Converter</h1>
-            <a
-                href={`https://medium.com/@${Constants.USERNAME}/latest?format=json`}
-                target='_source'
-            >
-                Go here and click cmd + a to select everything
-            </a>
+            <MediumJsonLink />
             <form onSubmit={handleSubmit} >
                 <label htmlFor='raw-medium-text-input'>Paste your Medium text into here:</label>
                 <textarea
