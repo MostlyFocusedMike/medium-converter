71b5a445da86f89e15d1a02e4ac65d6a4ea4293d
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-10-02 23:45:46 -0400

    add query params to get tags in articles

--- src/backend/lib/routes/article/list.js
+++ src/backend/lib/routes/article/list.js
@@ -1,9 +1,17 @@
+const Joi = require('joi');
+
 module.exports = {
     method: 'GET',
     path: '/articles',
     options: {
+        validate: {
+            query: {
+                tags: Joi.boolean().optional().description('Whether or not you want tags included with each article'),
+            },
+        },
         handler: async (request, h) => {
             const { Article } = request.server.app.Database;
+            if (request.query.tags) return Article.getArticlesWithTags();
             return Article.all();
         },
     },
